7b6279255e25ffd281b154c051f8dbe16f3af57a
Wow <160201362+woo-oow@users.noreply.github.com>
2024-03-15 22:32:48 -0700

    Update script.js

--- script.js
+++ script.js
@@ -2,3 +2,18 @@ let nums = []
 function saven(n) {
   nums.push(n)
 }
+function add() {
+  if (nums > 2) { return nums[nums.length() - 1] + nums[nums.length() - 2] }
+}
+function sub() {
+  if (nums > 2) { return nums[nums.length() - 1] - nums[nums.length() - 2] }
+}
+function mul() {
+  if (nums > 2) { return nums[nums.length() - 1] * nums[nums.length() - 2] }
+}
+function div() {
+  if (nums > 2) { return nums[nums.length() - 1] / nums[nums.length() - 2] }
+}
+function square() {
+  if (nums > 2) { return nums[nums.length() - 1] * nums[nums.length() - 1] }
+}
