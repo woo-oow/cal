6f5039ce84dab617d3a8cfa03ae4efdf4fa1bb35
Wow <160201362+woo-oow@users.noreply.github.com>
2024-03-18 17:03:36 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,25 @@
-let nums = []
+let nums1 = []
+let nums2 = []
+
+var num1 = document.getElementById("num1")
+var num2 = document.getElementById("num2")
+
+var op = document.getElementById("op")
 function saven(n) {
-  nums.push(n)
+  nums1.push(n)
 }
 function add() {
-  if (nums > 2) { return nums[nums.length() - 1] + nums[nums.length() - 2] }
+  if (nums1 > 2) { return nums1[nums1.length() - 1] + nums2[nums2.length() - 2] }
 }
 function sub() {
-  if (nums > 2) { return nums[nums.length() - 1] - nums[nums.length() - 2] }
+  if (nums1 > 2) { return nums1[nums1.length() - 1] - nums2[nums2.length() - 2] }
 }
 function mul() {
-  if (nums > 2) { return nums[nums.length() - 1] * nums[nums.length() - 2] }
+  if (nums1 > 2) { return nums1[nums1.length() - 1] * nums2[nums2.length() - 2] }
 }
 function div() {
-  if (nums > 2) { return nums[nums.length() - 1] / nums[nums.length() - 2] }
+  if (nums1 > 2) { return nums1[nums1.length() - 1] / nums2[nums2.length() - 2] }
 }
 function square() {
-  if (nums > 2) { return nums[nums.length() - 1] * nums[nums.length() - 1] }
+  if (nums1 > 2) { return nums1[nums1.length() - 1] * nums2[nums1.length() - 1] }
 }
