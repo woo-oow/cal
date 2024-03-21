d8740d20fbcf76edcb990b586b9994aeae88486d
Wow <160201362+woo-oow@users.noreply.github.com>
2024-03-20 17:30:36 -0700

    Update script.js

--- script.js
+++ script.js
@@ -8,27 +8,42 @@ var num1 = document.getElementById("num1")
 var num2 = document.getElementById("num2")
 
 var op = document.getElementById("op")
+
 function saven(n) {
   nums1.push(n)
 }
 function add() {
-  if (nums1 > 2) { return nums1[nums1.length() - 1] + nums2[nums2.length() - 2] }
+  if (nums1 > 2) { 
+    return nums1[nums1.length() - 1] + nums2[nums2.length() - 2]
+  }
 }
 function subtract() {
-  if (nums1 > 2) { return nums1[nums1.length() - 1] - nums2[nums2.length() - 2] }
+  if (nums1 > 2) { 
+    return nums1[nums1.length() - 1] - nums2[nums2.length() - 2] 
+  }
 }
 function multiply() {
-  if (nums1 > 2) { return nums1[nums1.length() - 1] * nums2[nums2.length() - 2] }
+  if (nums1 > 2) { 
+    return nums1[nums1.length() - 1] * nums2[nums2.length() - 2] 
+  }
 }
 function divide() {
-  if (nums1 > 2) { return nums1[nums1.length() - 1] / nums2[nums2.length() - 2] }
+  if (nums1 > 2) { 
+    return nums1[nums1.length() - 1] / nums2[nums2.length() - 2] 
+  }
 }
 function square() {
-  if (nums1 > 2) { return nums1[nums1.length() - 1] * nums2[nums1.length() - 1] }
+  if (nums1 > 2) { 
+    return nums1[nums1.length() - 1] * nums2[nums1.length() - 1] 
+  }
 }
-
-distring1 = nums1.join("");
-distring2 = nums2.join("");
+function clear() {
+  let nums1 = []
+  let nums2 = []
+}
+nums1.forEach((i) => {
+  distring = distring + i
+});
 
 num1.value = distring1
 num2.value = distring2
